e44fd5816c2a222e34e8e28670ab244c1bcd03c5
Jeffreyvh <jeffreyvh@ymail.com>
2019-01-16 14:34:24 -0600

    updated build

---
+++ client/build/precache-manifest.cf6af6c0efe9b085f83dcea676d046b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b7f7804bef99982a9e12",
+    "url": "/static/js/main.b7f7804b.chunk.js"
+  },
+  {
+    "revision": "a970f8150056aee650f3",
+    "url": "/static/js/1.a970f815.chunk.js"
+  },
+  {
+    "revision": "b7f7804bef99982a9e12",
+    "url": "/static/css/main.f242b7ac.chunk.css"
+  },
+  {
+    "revision": "92eeb18cf6f7aacdc237e34559a3ac4b",
+    "url": "/index.html"
+  }
+];
